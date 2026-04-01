9b5a74de3ccdd5c506540c91bf0d573fdeea9544
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-04-01 18:05:23 +0700

    config.php

--- config.php
+++ config.php
@@ -1,73 +1,23 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$host = $_ENV['DB_HOST'] ?? 'localhost';
-$port = (int)($_ENV['DB_PORT'] ?? 3306);
-$user = $_ENV['DB_USER'] ?? 'root';
-$pass = $_ENV['DB_PASS'] ?? '';
-$db   = $_ENV['DB_NAME'] ?? 'simklinik';
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = mysqli_init();
+
+if (!$conn) {
+    die("mysqli init gagal");
+}
+
+/* SSL WAJIB untuk DigitalOcean */
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, (int)$port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("DB CONNECT ERROR: " . mysqli_connect_error());
+}
+
 $conn->set_charset('utf8mb4');
-
-/*
- |------------------------------------------------------------
- | SSL untuk cloud database
- |------------------------------------------------------------
- | Aman dipanggil juga di lokal. Jika server cloud butuh SSL,
- | koneksi akan tetap siap.
- */
-@$conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
-
-if ($conn->connect_error) {
-    die("DB ERROR: " . $conn->connect_error);
-}
-
-/*
- |------------------------------------------------------------
- | Konfigurasi identitas klinik
- |------------------------------------------------------------
-*/
-if (!defined('LOGO_KLINIK')) {
-    define('LOGO_KLINIK', __DIR__ . '/assets/logo-klinik.png');
-}
-
-if (!defined('QRIS_IMAGE')) {
-    define('QRIS_IMAGE', __DIR__ . '/assets/qris.png');
-}
-
-if (!defined('NAMA_KLINIK')) {
-    define('NAMA_KLINIK', 'Poli Gigi');
-}
-
-if (!defined('TAGLINE_KLINIK')) {
-    define('TAGLINE_KLINIK', 'Praktek Mandiri Dokter Gigi Andreas Aryo');
-}
-
-if (!defined('ALAMAT_KLINIK')) {
-    define('ALAMAT_KLINIK', 'Bukit Nusa Indah 77');
-}
-
-if (!defined('TELP_KLINIK')) {
-    define('TELP_KLINIK', '08111-18-17-18');
-}
-
-if (!defined('EMAIL_KLINIK')) {
-    define('EMAIL_KLINIK', 'tigadental@gmail.com');
-}
-
-if (!defined('DOKTER_KLINIK')) {
-    define('DOKTER_KLINIK', 'drg. Andreas Aryo R.P');
-}
-
-if (!defined('SIP_DOKTER')) {
-    define('SIP_DOKTER', 'SIP.446/DRG/1/440-CPMptsp/2024');
-}
-
-if (!defined('BANK_KLINIK')) {
-    define('BANK_KLINIK', 'Transfer Bank BCA 4780209661 / Tunai / Debit / QRIS');
-}
-
-if (!defined('QRIS_INFO')) {
-    define('QRIS_INFO', 'Scan QRIS tersedia di resepsionis');
-}
