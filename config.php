78eabc34d828e71dc0880d2dcc70ab1d12b5489b
User <user@tigadental.local>
2026-04-02 15:29:29 +0700

    clean config production

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "CONFIG START<br>";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $host = getenv("DB_HOST") ?: "localhost";
 $user = getenv("DB_USER") ?: "root";
@@ -10,17 +7,10 @@ $pass = getenv("DB_PASS") ?: "";
 $db   = getenv("DB_NAME") ?: "simklinik";
 $port = (int)(getenv("DB_PORT") ?: 3306);
 
-echo "HOST=$host<br>";
-echo "PORT=$port<br>";
-echo "USER=$user<br>";
-echo "DB=$db<br>";
-
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Database error: " . $conn->connect_error);
 }
-
-echo "DB OK<br>";
 
 $conn->set_charset("utf8mb4");
