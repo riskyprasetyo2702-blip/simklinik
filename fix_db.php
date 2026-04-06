ef91c2af1b2cb1cb5dc466a3ffa60dcf56c153ba
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-04-06 17:05:09 +0700

    Update fix_db.php

--- fix_db.php
+++ fix_db.php
@@ -1,15 +1,26 @@
 <?php
 require_once 'config.php';
 
+mysqli_report(MYSQLI_REPORT_OFF);
+
 echo "Start Fix...<br>";
 
-$conn->query("ALTER TABLE odontogram_tindakan MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT");
-echo "odontogram_tindakan OK<br>";
+if (!isset($conn) || !($conn instanceof mysqli)) {
+    die("Koneksi database tidak valid.");
+}
 
-$conn->query("ALTER TABLE invoice MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT");
-echo "invoice OK<br>";
+function runQuery($conn, $sql, $label) {
+    echo "Menjalankan: $label ...<br>";
+    $ok = $conn->query($sql);
+    if ($ok) {
+        echo "$label OK<br>";
+    } else {
+        echo "$label GAGAL: " . $conn->error . "<br>";
+    }
+}
 
-$conn->query("ALTER TABLE invoice_items MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT");
-echo "invoice_items OK<br>";
+runQuery($conn, "ALTER TABLE odontogram_tindakan MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT", "odontogram_tindakan");
+runQuery($conn, "ALTER TABLE invoice MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT", "invoice");
+runQuery($conn, "ALTER TABLE invoice_items MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT", "invoice_items");
 
 echo "DONE";
