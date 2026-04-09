9ed85f691c3b91c03390be7a5dd33b626ffa6074
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-04-09 13:18:41 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -300,6 +300,12 @@ body{
         <div class="top-actions">
             <a class="btn light" href="pasien.php">Data Pasien</a>
             <a class="btn secondary" href="laporan_keuangan.php">Laporan</a>
+        
+            <a href="settings_klinik.php" 
+               style="padding:10px 15px;background:#0f172a;color:white;border-radius:10px;text-decoration:none;">
+                ⚙️ Settings Klinik
+            </a>
+        </div>
             <a class="btn" href="logout.php">Logout</a>
         </div>
     </div>
