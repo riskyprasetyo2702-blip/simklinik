df0c845da9bf537ef727f6bfccd1b5a5048ec030
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-04-11 14:00:30 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,6 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
 
-echo 'LOGIN: ' . ($_SESSION['login'] ?? 'tidak ada') . '<br>';
-echo 'ROLE: ' . ($_SESSION['role'] ?? 'tidak ada') . '<br>';
-echo 'USER: ' . ($_SESSION['username'] ?? 'tidak ada');
-
 $conn = db();
 if (!$conn) {
     die('Koneksi database tidak tersedia.');
