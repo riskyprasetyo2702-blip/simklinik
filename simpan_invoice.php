20c5cc4c2a0d347686bf2c88e7a96b1f8b380a00
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-04-13 18:00:50 +0700

    Update simpan_invoice.php

--- simpan_invoice.php
+++ simpan_invoice.php
@@ -1,5 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/bootstrap.php';
+ensure_logged_in();require_once __DIR__ . '/bootstrap.php';
 ensure_logged_in();
 
 $conn = db();
